75abcb1995d270a36cd5caf1f2344e9e33cc45be
Nathan Rzepecki <nathan@lionslair.net.au>
2021-10-06 16:17:43 +0800

    add missing scope to the package

--- config/laravel-xero-oauth.php
+++ config/laravel-xero-oauth.php
@@ -4,7 +4,7 @@ return [
     'oauth' => [
         'client_id'     => env('XERO_CLIENT_ID', ''),
         'client_secret' => env('XERO_CLIENT_SECRET', ''),
-        'scopes'        => env('XERO_SCOPE', 'openid email profile accounting.settings accounting.transactions accounting.contacts offline_access payroll.payruns payroll.employees payroll.timesheets payroll.settings accounting.attachments'),
+        'scopes'        => env('XERO_SCOPE', 'openid email profile accounting.settings accounting.transactions accounting.contacts offline_access payroll.payruns payroll.employees payroll.timesheets payroll.settings payroll.settings.read accounting.attachments'),
     ],
 
     /*
