77f38b27885e9be2b535ed427f7a79ec6367b8ec
Nathan Rzepecki <nathan@lionslair.net.au>
2022-01-21 14:37:56 +0800

    No need to have the css and js included twice

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,12 +7,6 @@
 
     <title>Xero oAuth Settings</title>
 
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.11/tailwind.min.css">
-
 </head>
 <body class="font-sans antialiased">
 <div>
