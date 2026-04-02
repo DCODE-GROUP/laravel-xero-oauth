68e14f02089caac4c4fa264e6bb33965d42ba5f8
Josh Young <jny986@gmail.com>
2026-04-02 10:34:16 +1030

    Fix issue with duplicate user tables

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,47 +2,10 @@
 
 namespace Dcodegroup\LaravelXeroOauth\Tests;
 
-use Dcodegroup\LaravelXeroOauth\LaravelXeroOauthServiceProvider;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Support\Facades\Route;
+use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as BaseTestCase;
-use Workbench\App\Providers\WorkbenchServiceProvider;
 
 abstract class TestCase extends BaseTestCase
 {
-    use DatabaseMigrations;
-
-    protected function getPackageProviders($app)
-    {
-        return [
-            LaravelXeroOauthServiceProvider::class,
-            WorkbenchServiceProvider::class,
-        ];
-    }
-
-    protected function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('laravel-xero-oauth.multi_tenant_model', null);
-    }
-
-    protected function defineDatabaseMigrations()
-    {
-        // Load migrations from workbench
-        $this->loadMigrationsFrom([
-            __DIR__.'/../workbench/database/migrations',
-            __DIR__.'/../database/migrations',
-        ]);
-    }
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-    }
-
-    protected function defineRoutes($router)
-    {
-        // Load workbench web routes
-        Route::middleware('web')
-            ->group(__DIR__.'/../workbench/routes/web.php');
-    }
+    use WithWorkbench;
 }
