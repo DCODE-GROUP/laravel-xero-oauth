b51132f55843354a5e08a2dc606460f29357ee13
Nathan Rzepecki <nathan@lionslair.net.au>
2026-04-07 16:52:08 +0800

    style(XeroAuthController): added Response type for Inertia frontend driver

--- src/Http/Controllers/XeroAuthController.php
+++ src/Http/Controllers/XeroAuthController.php
@@ -3,8 +3,9 @@
 namespace Dcodegroup\LaravelXeroOauth\Http\Controllers;
 
 use Calcinai\OAuth2\Client\Provider\Xero;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Controller;
+use RuntimeException;
+use Symfony\Component\HttpFoundation\Response;
 
 class XeroAuthController extends Controller
 {
@@ -18,10 +19,20 @@ class XeroAuthController extends Controller
         $this->xeroClient = $xero;
     }
 
-    public function __invoke(): RedirectResponse
+    public function __invoke(): Response
     {
-        return redirect()->to($this->xeroClient->getAuthorizationUrl([
+        $authorizationUrl = $this->xeroClient->getAuthorizationUrl([
             'scope' => [config('laravel-xero-oauth.oauth.scopes')],
-        ]));
+        ]);
+
+        if (config('laravel-xero-oauth.frontend.driver') === 'inertia') {
+            if (! class_exists(\Inertia\Inertia::class)) {
+                throw new RuntimeException('Inertia frontend driver is configured but inertiajs/inertia-laravel is not installed.');
+            }
+
+            return \Inertia\Inertia::location($authorizationUrl);
+        }
+
+        return redirect()->to($authorizationUrl);
     }
 }
